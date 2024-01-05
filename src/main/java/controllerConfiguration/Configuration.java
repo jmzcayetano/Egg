5351fc1e826ab4fed3537f83cd68e45fb324a436
jmzcayetano <jmzcayetano@gmail.com>
2024-01-04 23:40:53 -0500

    Mi primer Commit

---
+++ src/main/java/controllerConfiguration/Configuration.java
@@ -0,0 +1,13 @@
+package controllerConfiguration;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Configuration {
+	
+	
+	public String Saludo() {
+		return "Hola";
+	}
+
+}
